ff155e859334027a08bcd58820e3de7d77bc9343
Vaibhav Gupta <guptavaibhav0@gmail.com>
2017-01-12 20:11:35 -0800

    animation and wrapper updates
    Animation - allow changing and closing windows during animation
    
    Wrapper - ability to change model parameters and to switch between
    graphics display

--- MATLAB/ReDySim/Wrapper V1/cognateWrapper.m
+++ MATLAB/ReDySim/Wrapper V1/cognateWrapper.m
@@ -1,28 +1,55 @@
-function [] = cognateWrapper( theta2 )
+function [] = cognateWrapper(theta2, varargin)
+    p = inputParser;
+    
+    defaults.l1 = 3;
+    defaults.l2 = 1;
+    defaults.l3 = 2;
+    defaults.l4 = 3;
+    defaults.alpha = pi/2;
+    defaults.beta = pi/3;
+    defaults.displayGraphics = true;
+    
+    addRequired(p,'theta2',@isnumeric);
+    addOptional(p,'l1',defaults.l1,@isnumeric);
+    addOptional(p,'l2',defaults.l2,@isnumeric);
+    addOptional(p,'l3',defaults.l3,@isnumeric);
+    addOptional(p,'l4',defaults.l4,@isnumeric);
+    addOptional(p,'alpha',defaults.alpha,@isnumeric);
+    addOptional(p,'beta',defaults.beta,@isnumeric);
+    
+    addParameter(p,'displayGraphics',defaults.displayGraphics,...
+                 @islogical);
+    
+    
+    parse(p,theta2,varargin{:});
+
     clc;
     global cognateModel
-    cognateModel.l1 = 3;
-    cognateModel.l2 = 1;
-    cognateModel.l3 = 2;
-    cognateModel.l4 = 1;
-    cognateModel.alpha = pi/2;
-    cognateModel.beta = pi/3;
-    cognateModel.theta2 = theta2;
+    cognateModel.l1 = p.Results.l1;
+    cognateModel.l2 = p.Results.l2;
+    cognateModel.l3 = p.Results.l3;
+    cognateModel.l4 = p.Results.l4;
+    cognateModel.alpha = p.Results.alpha;
+    cognateModel.beta = p.Results.beta;
+    cognateModel.theta2 = p.Results.theta2;
     
     get_initial_thetas();
     cognateModel.dth=[0; 0;  0;    0;  0;  0;  0;  0;  0];
     cognateModel.act_en=0;
 
     runfor;
-    %Plots the joint motion
-    plot_motion;
-    %Plots the joint accelerations and joint torques
-    plot_acc_tor;
-    %Energy Calculation
-    energy;
-    %Plots Energy Balance
-    plot_en;
-    % Animation
-    animate
+    
+    if (p.Results.displayGraphics)
+        %Plots the joint motion
+        plot_motion;
+        %Plots the joint accelerations and joint torques
+        plot_acc_tor;
+        %Energy Calculation
+        energy;
+        %Plots Energy Balance
+        plot_en;
+        % Animation
+        animate
+    end
 end
 
