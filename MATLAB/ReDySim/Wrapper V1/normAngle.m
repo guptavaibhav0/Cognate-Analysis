ce34fabb9b76989b0999c978125852b0c84fa558
Vaibhav Gupta <guptavaibhav0@users.noreply.github.com>
2017-01-17 10:30:01 +0530

    normAngle correction

--- MATLAB/ReDySim/Wrapper V1/normAngle.m
+++ MATLAB/ReDySim/Wrapper V1/normAngle.m
@@ -1,10 +1,7 @@
 function [ang] = normAngle(ang)
     ang = double(vpa(ang));
-%     ang = mod(ang, 2*pi);
+    ang = mod(ang, 2*pi);
     if (ang > pi)
         ang = ang - 2*pi;
-    elseif (ang <0)
-        ang = ang + 2*pi;
     end
-
 end
